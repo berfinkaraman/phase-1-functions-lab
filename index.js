b40755360e4cc19b43dcb7d76e623c713a5acbb8
Berfin <berfin@minerva.kgi.edu>
2022-04-20 23:47:01 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(a) {
+    return Math.abs(a - 42);
+  }
+  
+  function distanceFromHqInFeet(a) {
+    return distanceFromHqInBlocks(a) * 264;
+  }
+  
+  function distanceTravelledInFeet(a, b) {
+    return Math.abs(a - b) * 264;
+  }
+  
+  function calculatesFarePrice(a, b) {
+    let total = distanceTravelledInFeet(a, b);
+    if (total <= 400) {
+      return 0;
+    } else if (total > 400 && total < 2000) {
+      total = total - 400;
+      return total * 0.02;
+    } else if (total > 2500) {
+      return "cannot travel that far";
+    } else if (total > 2000) {
+      return 25;
+    }
+  }
